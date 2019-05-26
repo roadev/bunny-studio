2aa3d54e19ab47a1cfbd30e54cb32f908adc98dd
Juan Roa <jdroa92@gmail.com>
2019-05-26 12:14:14 -0500

    :tada: initial commit

---
+++ packages/bunny-studio-app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = (): JSX.Element => {
+	return (
+		<div>
+			<h3>Hi Bunny Studio!</h3>
+		</div>
+	);
+};
+
+export default App;
---
+++ packages/bunny-studio-app/webpack.config.js
@@ -0,0 +1,47 @@
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.min.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			},
+			{
+				test: /\.(png|svg|jpg|gif|pdf)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				]
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.template.html'
+		})
+	]
+};
